34036052b116e0cc73d60bceb464815c2fa926b7
jkspam2004 <jkspam2004@yahoo.com>
2016-08-30 02:17:51 -0700

    add login/registation functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+  # relationships
+
+  # validations
   has_secure_password
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :first_name, :last_name, :handle, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+  validates :password, length: { minimum: 8 }
+  validates :password_confirmation, :presence => true
 end
