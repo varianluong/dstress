9d64899f53a361360e1264c3cb5b7873f5e68c0b
umaslakshmi <umaslakshmi@gmail.com>
2016-08-31 16:24:17 -0700

    created results page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,17 @@
+require 'json'
+require 'net/http'
+require 'open-uri'
 class ArticlesController < ApplicationController
 	def index
-		url = "https://www.googleapis.com/customsearch/v1?key=#{Figaro.env.GOOGLE_API_KEY}&cx=017576662512468239146:omuauf_lfve&q=lectures"
+		url = "https://www.googleapis.com/customsearch/v1?key=#{Figaro.env.GOOGLE_API_KEY}&cx=#{Figaro.env.GOOGLE_CUSTOM_SEARCH_CX}&q=cat+videos&fields=items"
+		uri = URI.parse(url)
+		http = Net::HTTP.new(uri.host, uri.port)
+		http.use_ssl = true
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+		request = Net::HTTP::Get.new(uri.request_uri)
+		response = http.request(request)
+		data = response.body
+		items = JSON.load(data)
+		@results = items['items']
 	end
 end
