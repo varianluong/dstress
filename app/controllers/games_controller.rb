bd9edd114ec5caaf418c19a1383649e366c33d96
Varian Luong <varianluong@yahoo.com>
2016-08-31 16:54:01 -0700

    Fixed a bug

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,13 @@
 class GamesController < ApplicationController
   def index
-  	@user = User.find(session[:user_id]) 
+  	user = User.find(session[:user_id]) 
   	Image.first.destroy if (Image.first != Image.last) 
-  	@image = Image.last
-  	@image.update(user_id: session[:user_id])
+  	image = Image.last
+  	#@image.update(user_id: session[:user_id])
+  	if (image === nil)
+  		@imgurl = "/assets/the-trump.jpg"
+  	else
+  		@imgurl = image.dartboard.url
+  	end
   end
 end
