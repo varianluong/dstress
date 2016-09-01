45b6a4757e375f9f7e0e94997895da5dba746d2a
umaslakshmi <umaslakshmi@gmail.com>
2016-09-01 09:46:19 -0700

    updated google search

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,15 +3,15 @@ require 'net/http'
 require 'open-uri'
 class ArticlesController < ApplicationController
 	def index
-		url = "https://www.googleapis.com/customsearch/v1?key=#{Figaro.env.GOOGLE_API_KEY}&cx=#{Figaro.env.GOOGLE_CUSTOM_SEARCH_CX}&q=cat+videos&fields=items"
-		uri = URI.parse(url)
-		http = Net::HTTP.new(uri.host, uri.port)
-		http.use_ssl = true
-		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-		request = Net::HTTP::Get.new(uri.request_uri)
-		response = http.request(request)
-		data = response.body
-		items = JSON.load(data)
-		@results = items['items']
+		# url = "https://www.googleapis.com/customsearch/v1?key=#{Figaro.env.GOOGLE_API_KEY}&cx=#{Figaro.env.GOOGLE_CUSTOM_SEARCH_CX}&q=cat+videos&fields=items"
+		# uri = URI.parse(url)
+		# http = Net::HTTP.new(uri.host, uri.port)
+		# http.use_ssl = true
+		# http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+		# request = Net::HTTP::Get.new(uri.request_uri)
+		# response = http.request(request)
+		# data = response.body
+		# items = JSON.load(data)
+		# @results = items['items']
 	end
 end
